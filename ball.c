0db88ca449f40b014330d41dbc7ecfc50034f4c7
Yigit Gunduc <ygunduc@gmail.com>
2021-11-23 14:57:46 +0300

    partially working but not really

--- ball.c
+++ ball.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define WIDTH 8
-#define HEIGHT 8
+#define HEIGHT 16
 #define RADIUS 3
+
+
+
+void clearScreen()
+{
+    const char *CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
+      write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
+}
 
 char board[WIDTH][HEIGHT];
 
@@ -12,8 +22,9 @@ typedef struct {
 } pos;
 
 pos ball_center = {.x = 2, .y = 2};
+pos ball_speed = {.x = 2, .y = 2};
 
-void compute_ball_pos(void)
+void update_canvas(void)
 {
   for (int i = 0; i < WIDTH; i++)
   {
@@ -25,12 +36,8 @@ void compute_ball_pos(void)
   board[ball_center.x][ball_center.y] = 'x';
 }
 
-
-int main(void)
+void draw_canvas(void)
 {
-  puts("start");
-  compute_ball_pos();
-  
   for (int i = 0; i < WIDTH; i++)
   {
     for (int j = 0; j < HEIGHT; j++)
@@ -39,6 +46,41 @@ int main(void)
     }
     printf("\n");
   }
+}
+
+void step(void)
+{
+  if (ball_center.x >= WIDTH)
+  {
+    ball_speed.x *= -1;
+  }
+
+  if (ball_center.y >= HEIGHT)
+  {
+    ball_speed.y *= -1;
+  }
+  ball_center.x += ball_speed.x;
+  ball_center.y += ball_speed.y;
+}
+
+void run(void)
+{
+  for (;;)
+  {
+  update_canvas();
+  //draw_canvas();
+  printf("\n");
+  printf("x: %i, y: %i", ball_center.x, ball_center.y);
+  step();
+  sleep(1);
+  }
+}
+
+
+int main(void)
+{
+  
+  run();
   // printf("x : %i\n", ball_center.x);
 
 
